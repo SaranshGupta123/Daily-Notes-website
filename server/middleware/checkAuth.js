2344a9552ea81f0ddfc93691916beba977c27c4d
Saransh gupta <73605084+SaranshGupta123@users.noreply.github.com>
2024-11-05 20:48:02 +0530

    Update checkAuth.js

--- server/middleware/checkAuth.js
+++ server/middleware/checkAuth.js
@@ -1,7 +1,9 @@
+// server/middleware/checkAuth.js
+
 exports.isLoggedIn = function (req, res, next) {
-  if(req.user) {
-    next();
+  if (req.user) {
+    next(); // User is authenticated, proceed to the next middleware or route
   } else {
-    return res.status(401).send('Access Denied');
+    return res.status(401).send('Access Denied'); // User is not authenticated, send a 401 response
   }
-}
+};
